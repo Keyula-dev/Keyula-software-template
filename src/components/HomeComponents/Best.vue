449cf44fe16ab146f3428e39ec04e9b1b29f041e
Esereomo-BackendPapa <akejupaul10@gmail.com>
2021-01-21 09:23:00 +0100

    best.vue

---
+++ src/components/HomeComponents/Best.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <v-row align="center" justify="center">
+          <v-col cols="12" xs="12" sm="6" md="6" lg="6" xl="6">
+
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="6" xl="6">
+              <div>
+                  <p class="font-weight-bold text-h4">Get the best package for your business</p>
+                  <hr style="height:3px;width:15%;" class="blue darken-4">
+                  <br>
+
+                  <p style="color:#1A237E;" class="text-caption font-weight-bold">We work systematically to integrate corporate responsibility in our core business and make our expertise availablefor the benefit ofthe societies where we operate.</p>
+
+                  <p style="color:#1A237E;" class="text-caption font-weight-bold"><v-icon color="#1A237E"> mdi-checkbox-marked-circle-outline</v-icon >   Application that is easy to use and easy for users to understand</p>
+                  <p style="color:#1A237E;" class="text-caption font-weight-bold"><v-icon color="#1A237E"> mdi-checkbox-marked-circle-outline</v-icon >   We have supported libraries that libraries need</p>
+                  <p style="color:#1A237E;" class="text-caption font-weight-bold"><v-icon color="#1A237E">mdi-checkbox-marked-circle-outline</v-icon>    Perfect for large sites or agencies that manage many clients</p>
+              </div>
+          </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
     <Why />
+    <v-row align="center" justify="center">
+      <v-col cols="1" xs="12" sm="2" md="2" lg="1" xl="1" />
+      <v-col cols="10" xs="12" sm="8" md="8" lg="8" xl="8">
+        <Best />
+      </v-col>
+      <v-col cols="1" xs="12" sm="1" md="1" lg="1" xl="1" />
+    </v-row>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Why from '../components/HomeComponents/Why'
+import Best from '../components/HomeComponents/Best'
 
 export default {
   name: 'Home',
   components: {
-    Why
+    Why,
+    Best
   }
 }
 </script>
